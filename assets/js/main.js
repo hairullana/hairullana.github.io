7b482a89772d173da122cb1b0eda9c638e16a086
Hairul Lana <hairullana99@gmail.com>
2023-04-19 10:43:19 +0800

    dynamic period of works

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,9 @@
 	Stellar by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+
+	Modified by Hairul Lana
+	hairullana99@gmail.com
 */
 
 (function($) {
@@ -119,5 +122,46 @@
 		$('.scrolly').scrolly({
 			speed: 1000
 		});
+
+	// get work period
+	const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+	let work_periods = $('.work-period');
+
+	for (i=0; i<work_periods.length; i++) {
+		let work_period = work_periods[i];
+		let period = $(work_period).html();
+		let [start, end] = period.split(' - ');
+
+		let [start_month, start_year] = start.split(' ');
+		start_month = months.indexOf(start_month);
+
+		let end_month, end_year;
+		if (end == 'Present') {
+			let now = new Date();
+			end_month = now.getMonth();
+			end_year = now.getFullYear();
+		} else {
+			[end_month, end_year] = end.split(' ');
+			end_month = months.indexOf(end_month);
+		}
+
+		let start_date = new Date(start_year, start_month);
+		let end_date = new Date(end_year, end_month);
+		let year_diff = end_date.getFullYear() - start_date.getFullYear();
+		let month_diff = end_date.getMonth() - start_date.getMonth();
+		const total_month_diff = (year_diff * 12 + month_diff) + 1;
+
+		let fix_year, fix_month;
+		if (total_month_diff >= 12) {
+			fix_year = Math.ceil(total_month_diff / 12);
+			fix_month = total_month_diff % 12;
+
+			if (fix_month > 0) work_period.append(` (${fix_year} year${fix_year != 1 ? 's' : ''} ${fix_month} month${fix_month != 1 ? 's' : ''})`);
+			else work_period.append(` (${fix_year} year${fix_year != 1 ? 's' : ''})`);
+		} else {
+			fix_month = total_month_diff;
+			work_period.append(` (${fix_month} month${fix_month != 1 ? 's' : ''})`);
+		}
+	};
 
 })(jQuery);
